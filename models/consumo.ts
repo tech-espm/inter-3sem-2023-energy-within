07a5fae294d9a12901e48208ca05f826f948c6d2
joaopedrocaldas <jpecaldas@gmail.com>
2023-10-26 08:40:51 -0300

    models listar

--- models/consumo.ts
+++ models/consumo.ts
@@ -5,5 +5,18 @@ interface Consumo {
     id_eletrodomestico: number;
     data: string;
     consumo: number;
+    nome_eletrodomestico: string;
+}
 
+class Consumo {
+    public static async listarConsumos() {
+        let lista: Consumo[] = [];
+        await app.sql.connect(async (sql: app.Sql) => {
+            lista = await sql.query(`
+            select c.consumo, e.nome_eletrodomestico
+                FROM consumo c inner join eletrodomestico e
+                on c.id_eletrodomestico = e.id_eletrodomestico
+                GROUP BY c.id_eletrodomestico`)
+        });
+    }
 }
